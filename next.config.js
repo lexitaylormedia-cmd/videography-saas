fec6c4da39db540f0111ff93a3ef194249fc9a5f
Lexi Tickhill <lexitickhill@Mac.attlocal.net>
2025-08-22 14:55:21 -0400

    chore: disable lightningcss optimizeCss for build stability

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Helpful while we stabilize the project. Remove later if you want stricter builds.
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  experimental: {
+    // Turn off LightningCSS minifier to avoid native binding issues on some hosts
+    optimizeCss: false,
+  },
+  // optional: if you use next/font with remote fonts and see warnings, you can also disable font optimization
+  // optimizeFonts: false,
 };
 
 module.exports = nextConfig;
